36e41a8d3170696a4d166384b1e40c7312a03118
Xieryy <cuberjohnny9@gmail.com>
2025-10-25 08:39:37 +0700

    DoneStep3

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    This is my first VueJs project
+  </header>
+
+
+</template>
+
