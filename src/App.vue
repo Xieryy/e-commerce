b83185e6ffdad1d93ec0d72f7a01377502e6caf2
Xieryy <cuberjohnny9@gmail.com>
2026-01-22 21:22:07 +0700

    apply click event and insall axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import axios from 'axios'
+import axios from 'axios';
 import CategoryList from "@/components/categoryComp.vue";
 import PromoSection from "@/components/promotionComp.vue";
 
